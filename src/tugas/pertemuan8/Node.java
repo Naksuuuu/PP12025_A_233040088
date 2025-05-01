8017ff3483e5328e716e049865f79770dd55499b
Ariel Wijaya <arielkusuma17@gmail.com>
2025-05-01 15:51:25 +0700

    Tugas untuk pertemuan 8

---
+++ src/tugas/pertemuan8/Node.java
@@ -0,0 +1,31 @@
+package pertemuan8;
+
+public class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getNilai() {
+        return data;
+    }
+
+
+}
+
+
